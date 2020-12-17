b91e7307afaa64fa6319729426fd156bdc67af83
Renan-Bodom <renan1211@hotmail.com>
2020-12-17 10:13:59 -0300

    inicial index

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function btnAlter(){
+    //var btn = document.createElement("div");
+    var alter = document.getElementById("AltOuDis");
+    alter.innerHTML = "<center>Teste!</center>";
+    //document.getElementById("AltOuDis").appendChild(<input type="text" name="alternativas"> <button class="add_field_button">+</button>);
+    document.append(alter);
+}
