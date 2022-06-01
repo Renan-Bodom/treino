cf0f79700ab47c8dc95160eabc8eebc70d1b92d0
Renan <renan1211@hotmail.com>
2022-06-01 13:27:32 -0300

    Correções e arquivo CSS

--- HTML/js/script.js
+++ HTML/js/script.js
@@ -1,7 +1,15 @@
 function btnAlter(){
     //var btn = document.createElement("div");
     var alter = document.getElementById("AltOuDis");
-    alter.innerHTML = "<center>Teste!</center>";
+    alter.innerHTML = "<center>Apertou alternativas!</center>";
     //document.getElementById("AltOuDis").appendChild(<input type="text" name="alternativas"> <button class="add_field_button">+</button>);
-    document.append(alter);
+    //document.append(alter);
+}
+
+function btnDiss(){
+    //var btn = document.createElement("div");
+    var diss = document.getElementById("AltOuDis");
+    diss.innerHTML = "<center>Apertou dissertativa!</center>";
+    //document.getElementById("AltOuDis").appendChild(<input type="text" name="alternativas"> <button class="add_field_button">+</button>);
+    //document.append(diss);
 }
